4efac7d64b5ea6d7e013b6a599425930bedff6fb
mikeymop <midefran@gmail.com>
2019-05-31 10:22:49 -0400

    made the code more error resistant, scale and bg are now optional fields

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import sys, os, tempfile, logging, segno, flask
 
 logger = logging.getLogger()
 
-def mqr_gen(request):
+def qr_gen(request):
+	background = None
 	qrtext = flask.request.args.get('text') # gets query string params
 	scale = flask.request.args.get('scale')
+	background = flask.request.args.get('bg')
 	print("Got %s %s" % (str(qrtext), str(scale)))
+	if(scale == None):
+		print("Defaulting scale to 1")
+		scale = 1
 
 	temp_dir = "/tmp"
 	os.chdir(temp_dir)
@@ -15,18 +20,18 @@ def mqr_gen(request):
 
 	# generate qrcode
 	print("Generating QR Code...")
-	mk_qr(result_file.name, scale, qrtext)
+	mk_qr(result_file.name, int(scale), qrtext, background)
 
 	# return in browser
 	print("Sending %s" % (str(result_file.name)))
 	response = flask.send_file(result_file, mimetype="image/png", as_attachment=False)
 	return response
 
-def mk_qr(ofile, qrsize, text):
+def mk_qr(ofile, qrsize, text, bg):
 	print(ofile, text)
 	qr = segno.make(str(text))
 	print("Saving qr code as %s" % (ofile))
-	qr.save(ofile, scale=qrsize, border=2, background=None)
+	qr.save(ofile, scale=qrsize, border=2, background=bg)
 
 """
 For testing locally
