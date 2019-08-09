8fb52fbf127bbff9b26b0db745a80e68c596b5a0
mikeymop <midefran@gmail.com>
2019-08-09 13:33:38 -0400

    Added support for other image types using PIL. Specifically for bmp/jpg support for SF

--- main.py
+++ main.py
@@ -7,34 +7,41 @@ def qr_gen(request):
 	qrtext = flask.request.args.get('text') # gets query string params
 	scale = flask.request.args.get('scale')
 	background = flask.request.args.get('bg')
+	ext = flask.request.args.get('ext')
 	print("Got %s %s" % (str(qrtext), str(scale)))
 	if(scale == None):
 		print("Defaulting scale to 1")
 		scale = 1
+	if(ext == None):
+		print("Defaulting to png")
+		ext = '.png'
 
-	temp_dir = "/tmp"
+	temp_dir = "/tmp" # this is the only writable location in gcloud
 	os.chdir(temp_dir)
 
-	# make temp files
-	result_file = tempfile.NamedTemporaryFile(mode="w+b",suffix='.png', dir=temp_dir, delete=True)
+	# make temp file
+	result_file = tempfile.NamedTemporaryFile(mode="w+b",suffix=ext, dir=temp_dir, delete=True)
 
 	# generate qrcode
 	print("Generating QR Code...")
-	mk_qr(result_file.name, int(scale), qrtext, background)
+	mk_qr(result_file.name, int(scale), qrtext, background, ext)
 
 	# return in browser
 	print("Sending %s" % (str(result_file.name)))
 	response = flask.send_file(result_file, mimetype="image/png", as_attachment=False)
 	return response
 
-def mk_qr(ofile, qrsize, text, bg):
+def mk_qr(ofile, qrsize, text, bg, ext):
 	print(ofile, text)
 	qr = segno.make(str(text))
 	print("Saving qr code as %s" % (ofile))
-	qr.save(ofile, scale=qrsize, border=2, background=bg)
+	if(ext == 'png'):
+		qr.save(ofile, scale=qrsize, border=2, background=bg)
+	else:
+		img = qr.to_pil(scale=scale, background=background)
+		img.save(ofile)
 
-"""
-For testing locally
-ofile, scale, text, result_bucket = sys.argv[1:]
-mk_qr(ofile, scale, text, result_bucket)
-"""
+
+# For testing locally
+ofile, scale, qrtext, background, ext = sys.argv[1:]
+mk_qr(ofile, int(scale), qrtext, background, ext)
